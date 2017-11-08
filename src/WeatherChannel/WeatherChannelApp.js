5b2c2cf1eb161b789228243f46a812478a7f0cf5
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-08 21:58:32 +0100

    Added hour display

--- src/WeatherChannel/WeatherChannelApp.js
+++ src/WeatherChannel/WeatherChannelApp.js
@@ -38,7 +38,11 @@ class WeatherChannelApp extends Component {
         {this.state.showdailyforcast ? (
           <div>
             <h1 className="App-title">Dagens väder</h1>
-            <button onClick={this.changeView}>back</button>
+            <div className="text-left">
+              <button className="btn btn-primary" onClick={this.changeView}>
+                Tillbaka
+              </button>
+            </div>
             <DailyForcast data={this.state.daytoshow} />{' '}
           </div>
         ) : (
@@ -57,5 +61,39 @@ export default WeatherChannelApp;
 const DailyForcast = props => {
   const { data } = props;
   console.log(data);
-  return <div>{data.day}</div>;
+  return (
+    <div>
+      {data.day}
+      <ul className="w-75 list-group center-block">
+        {data.time.map(hour_info => {
+          return <DailyForcastTime key={hour_info.time} data={hour_info} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const DailyForcastTime = props => {
+  const { time, info } = props.data;
+  const { temp, condition, wind, cloud_chance } = info;
+  let classes = 'badge badge-pill ';
+  classes += temp <= 0 ? 'badge-primary' : 'badge-warning';
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {time}
+      <div>
+        <span className={classes}>{temp}°C</span>
+        <br />
+        <span className="font-italic pr-2" style={{ fontSize: 10 }}>
+          condition: {condition}
+        </span>
+        <span className="font-italic pr-2" style={{ fontSize: 10 }}>
+          wind: {wind}
+        </span>
+        <span className="font-italic pr-2" style={{ fontSize: 10 }}>
+          cloud chance: {cloud_chance}
+        </span>
+      </div>
+    </li>
+  );
 };
--- src/WeatherChannel/weatherreport.js
+++ src/WeatherChannel/weatherreport.js
@@ -20,7 +20,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/13d.png',
       temp_hi: 5,
       temp_lo: -10,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        }
+      ]
     },
     {
       date: 20171107,
@@ -28,7 +61,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/01d.png',
       temp_hi: 2,
       temp_lo: 0,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        }
+      ]
     },
     {
       date: 20171108,
@@ -36,7 +102,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/01d.png',
       temp_hi: 10,
       temp_lo: 0,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        }
+      ]
     },
     {
       date: 20171109,
@@ -44,7 +143,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/03d.png',
       temp_hi: -2,
       temp_lo: -8,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        }
+      ]
     },
     {
       date: 20171110,
@@ -52,7 +184,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/13d.png',
       temp_hi: -10,
       temp_lo: -20,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        }
+      ]
     },
     {
       date: 20171111,
@@ -60,7 +225,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/01d.png',
       temp_hi: -20,
       temp_lo: -25,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '', cloud_chance: '' }
+        }
+      ]
     },
     {
       date: 20171112,
@@ -68,7 +266,40 @@ export const DATA_WEEKLY = {
       img: 'https://openweathermap.org/img/w/03d.png',
       temp_hi: -5,
       temp_lo: -10,
-      time: [{ '01:00': -10 }, { '04:00': -8 }, { '07:00': -5 }, { '10:00': 1 }, { '13:00': 5 }, { '16:00': 1 }, { '19:00': -5 }, { '22:00': -8 }]
+      time: [
+        {
+          time: '01:00',
+          info: { temp: -10, condition: 'Clear sky', wind: '7.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '04:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '8.55m/s', cloud_chance: '10%' }
+        },
+        {
+          time: '07:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '4.36m/s', cloud_chance: '80%' }
+        },
+        {
+          time: '10:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '5.10m/s', cloud_chance: '20%' }
+        },
+        {
+          time: '13:00',
+          info: { temp: 5, condition: 'Clear sky', wind: '5.62m/s', cloud_chance: '9%' }
+        },
+        {
+          time: '16:00',
+          info: { temp: 1, condition: 'Clear sky', wind: '1.62m/s', cloud_chance: '100%' }
+        },
+        {
+          time: '19:00',
+          info: { temp: -5, condition: 'Clear sky', wind: '8.62m/s', cloud_chance: '0%' }
+        },
+        {
+          time: '22:00',
+          info: { temp: -8, condition: 'Clear sky', wind: '70.62m/s', cloud_chance: '45%' }
+        }
+      ]
     }
   ]
 };
