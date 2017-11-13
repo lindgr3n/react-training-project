c2784b076414ddb253e350de1680567a9d1dc57c
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-13 22:28:19 +0100

    Option to open app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,39 @@ import WeatherChannelApp from './WeatherChannel/WeatherChannelApp';
 import CalculatorApp from './Calculator/CalculatorApp';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: ''
+    };
+  }
+  changeView(e) {
+    console.log(e.target);
+    this.setState({
+      view: e.target.className
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <WeatherChannelApp />
-        <hr />
-        <CalculatorApp />
+      <div>
+        <div className="App">
+          <div className="App-item one" onClick={e => this.changeView(e)}>
+            WeatherChannel
+          </div>
+          <div className="App-item two" onClick={e => this.changeView(e)}>
+            Calculator
+          </div>
+        </div>
+        <br className="clearfix" />
+        <div className="content">
+          <div className={this.state.view !== 'App-item one' ? 'hidden' : ''}>
+            <WeatherChannelApp />
+          </div>
+          <div className={this.state.view !== 'App-item two' ? 'hidden' : ''}>
+            <CalculatorApp />
+          </div>
+        </div>
       </div>
     );
   }
