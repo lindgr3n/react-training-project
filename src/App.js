f248bb7e37eb034967d1f0d445f75d5001e0e140
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-13 20:51:42 +0100

    First version calculatorapp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 import WeatherChannelApp from './WeatherChannel/WeatherChannelApp';
+import CalculatorApp from './Calculator/CalculatorApp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <WeatherChannelApp />
+        <hr />
+        <CalculatorApp />
       </div>
     );
   }
