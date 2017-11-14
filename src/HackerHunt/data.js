2ef86380526e90bb05335fa8d337f07b5402f851
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-14 14:48:48 +0100

    Add test data

---
+++ src/HackerHunt/data.js
@@ -0,0 +1,65 @@
+export const TOPICS_DATA = [
+  {
+    icon: 'rocket',
+    name: 'Development'
+  },
+  {
+    icon: 'tools',
+    name: 'System'
+  },
+  {
+    icon: 'data',
+    name: 'Daata science'
+  },
+  {
+    icon: 'chain',
+    name: 'Blockchain'
+  },
+  {
+    icon: 'mobile',
+    name: 'Mobile'
+  },
+  {
+    icon: 'star',
+    name: 'Awesome lists'
+  },
+  {
+    icon: 'social',
+    name: 'Social'
+  },
+  {
+    icon: 'frame',
+    name: 'Visual'
+  },
+  {
+    icon: 'beer',
+    name: 'Open source'
+  },
+  {
+    icon: 'chest',
+    name: 'All topics'
+  }
+];
+
+export const NEWSFEED_DATA = [
+  {
+    name: 'hiration.com',
+    descr: 'Professional profile management tol built using react',
+    modified: '6 hours ago',
+    modifiedby: 'anishakka',
+    tags: ['WEB', 'EXAMPLE', 'DEVELOPMENT'],
+    ups: 61,
+    comments: 28,
+    stared: true
+  },
+  {
+    name: 'gameofcoins.de',
+    descr: 'GameOfCoins.de (Virtual Cryptocurrency Trading',
+    modified: '3 hours ago',
+    modifiedby: 'erajob',
+    tags: ['SOCIAL', 'ENTERTAINMENT'],
+    ups: 27,
+    comments: 25,
+    stared: true
+  }
+];
