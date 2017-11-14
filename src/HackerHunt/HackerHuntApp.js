9564e5d902a937e31e5a61a7cc2e7a01140a52c1
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-14 14:49:04 +0100

    First topic

--- src/HackerHunt/HackerHuntApp.js
+++ src/HackerHunt/HackerHuntApp.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const HackerHuntApp = props => {
-  return <div>test</div>;
-};
+import { NEWSFEED_DATA, TOPICS_DATA } from './data';
+
+import './HackerHuntApp.css';
+
+class HackerHuntApp extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="header">Header</div>
+        <div className="sidebar">
+          <Topics data={TOPICS_DATA} />
+        </div>
+        <div className="content">
+          <h1>Main article area</h1>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default HackerHuntApp;
+
+const Topics = props => {
+  const { data } = props;
+  return <div>{data.map(e => <TopicItem key={e} item={e} />)}</div>;
+};
+
+const TopicItem = props => {
+  const { item } = props;
+  return <div>{item.name}</div>;
+};
