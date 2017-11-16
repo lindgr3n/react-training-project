afc7190b311dae6d54b44f7173dc9866a45eae97
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-17 00:14:01 +0100

    WIP neews feed

--- src/HackerHunt/HackerHuntApp.js
+++ src/HackerHunt/HackerHuntApp.js
@@ -23,7 +23,7 @@ class HackerHuntApp extends Component {
             <h3>Today</h3> <h3>Popular</h3>
           </header>
 
-          <h1>Main article area</h1>
+          <NeewsFeedList data={NEWSFEED_DATA} />
         </div>
 
         <div className="footer">footer</div>
@@ -36,10 +36,57 @@ export default HackerHuntApp;
 
 const Topics = props => {
   const { data } = props;
-  return <div>{data.map(e => <TopicItem key={e} item={e} />)}</div>;
+  return <div>{data.map(e => <TopicItem key={e.name} item={e} />)}</div>;
 };
 
 const TopicItem = props => {
   const { item } = props;
   return <div>{item.name}</div>;
+};
+
+const NeewsFeedList = props => {
+  const { data } = props;
+  return (
+    <div>
+      {data.map((newsItem, i) => {
+        return <NeewsItem key={i} item={newsItem} />;
+      })}
+    </div>
+  );
+};
+
+const NeewsItem = props => {
+  const { item } = props;
+  return (
+    <div className="news-container clearfix">
+      <div className="news-sidebar float-left p20">
+        <div className="news-ups">
+          <span>u</span>
+          {item.ups}
+        </div>
+        <div>
+          <span className="news-comments">c</span>
+          {item.comments}
+        </div>
+      </div>
+      <div className="news-content float-left">
+        <header>
+          {item.name}
+          <span>{item.stared}</span>
+        </header>
+        {item.descr}
+        <div className="news-footer">
+          {item.modified}
+          by {item.modifiedby}
+          {item.tags
+            ? item.tags.map(tag => (
+                <span className="news-tag" key={tag}>
+                  {tag}
+                </span>
+              ))
+            : ''}
+        </div>
+      </div>
+    </div>
+  );
 };
