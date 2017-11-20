b0d49b759e316c2795d46cf2855dc53e80d6a32c
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-21 00:05:16 +0100

    inital style copy

--- src/GithubIssues/GithubIssuesApp.js
+++ src/GithubIssues/GithubIssuesApp.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
+import './GithubIssuesApp.css';
+import './GithubIssuesApp2.css';
+
+import GithubHeader from './GithubHeader';
+import GithubContent from './GithubContent';
+import GithubFooter from './GithubFooter';
+
 class GithubIssuesApp extends Component {
   render() {
-    return <div>GithubIssues</div>;
+    return (
+      <div>
+        <GithubHeader />
+        <GithubContent />
+        <GithubFooter />
+      </div>
+    );
   }
 }
 
