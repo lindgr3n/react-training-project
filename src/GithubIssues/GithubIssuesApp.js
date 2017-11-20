7333da7a7e2aadc7e77d573315ef5c205a995ccf
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-21 00:22:31 +0100

    Just work with the list

--- src/GithubIssues/GithubIssuesApp.js
+++ src/GithubIssues/GithubIssuesApp.js
@@ -3,20 +3,24 @@ import React, { Component } from 'react';
 import './GithubIssuesApp.css';
 import './GithubIssuesApp2.css';
 
-import GithubHeader from './GithubHeader';
-import GithubContent from './GithubContent';
-import GithubFooter from './GithubFooter';
+// import GithubHeader from './GithubHeader';
+// import GithubContent from './GithubContent';
+// import GithubFooter from './GithubFooter';
+import GithubListContainer from './GIthubListContainer';
 
 class GithubIssuesApp extends Component {
   render() {
     return (
       <div>
-        <GithubHeader />
-        <GithubContent />
-        <GithubFooter />
+        <GithubListContainer />
       </div>
     );
   }
 }
 
+/*
+<GithubHeader />
+<GithubContent />
+<GithubFooter />
+*/
 export default GithubIssuesApp;
