336bdeb2a06c12845d138a9992daca0d713a0a28
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-23 22:58:19 +0100

    Working prototype

--- src/Increment/IncrementApp.js
+++ src/Increment/IncrementApp.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
 class IncrementApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  onDecrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+
   render() {
-    return <div>counter</div>;
+    return (
+      <div>
+        <ValueButton text="-" changeHandler={this.onDecrement} />
+        <Counter value={this.state.counter} />
+        <ValueButton text="+" changeHandler={this.onIncrement} />
+      </div>
+    );
   }
 }
+
+const Counter = props => {
+  const { value } = props;
+  return <input readOnly value={value} />;
+};
+
+const ValueButton = props => {
+  const { text, changeHandler } = props;
+  return <button onClick={changeHandler}>{text}</button>;
+};
 
 export default IncrementApp;
