0aa4ae5da234a21199bcbd7695765bac9ab1fab2
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-12-04 19:56:39 +0100

    Refactore

--- src/GithubIssues/GithubIssuesApp.js
+++ src/GithubIssues/GithubIssuesApp.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
 
-import './GithubIssuesApp.css';
-import './GithubIssuesApp2.css';
-
-// import GithubHeader from './GithubHeader';
-// import GithubContent from './GithubContent';
-// import GithubFooter from './GithubFooter';
-import GithubListContainer from './GIthubListContainer';
-
 class GithubIssuesApp extends Component {
   render() {
-    return (
-      <div>
-        <GithubListContainer />
-      </div>
-    );
+    return <div>Githubissues!</div>;
   }
 }
 
-/*
-<GithubHeader />
-<GithubContent />
-<GithubFooter />
-*/
 export default GithubIssuesApp;
